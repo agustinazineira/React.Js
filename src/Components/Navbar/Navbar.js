d3a5f8512bc8772075d5b9c5d774691e1145b56f
agustinazineira <agus2465@hotmail.com>
2022-11-20 13:39:53 -0300

    Agregue JSX

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <nav className='navbar navbar-dark bg-dark'>
+            <div className='container-fluid'>
+                <h1 className='text-light'>My App</h1>
+                <button className='btn btn-light'>Boton 1</button>
+                <button className='btn btn-light'>Boton 4</button>
+
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Navbar
