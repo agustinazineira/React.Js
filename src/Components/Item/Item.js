7656dd96ed89c7917409629ea9afff8246ce080e
agustinazineira <agus2465@hotmail.com>
2022-12-29 22:10:40 -0300

    cambios

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom"
 
-const Item = ({note}) => {
+
+const Item = ({product}) => {
+
     return(
         <div  className="d-flex flex-row mb-3">
             
             <div className="card p-1" style={{width:'18rem'}}> 
-            <img src={note.img} className="card-img-top" alt={note.title} />
+            <img src={product.img} className="card-img-top" alt={product.title} />
             <div>
-                <h2>{note.title} </h2>
-                <p>{note.category} </p>
-                <p>{note.text} </p>
-                <h3>${note.price} </h3>
-                <Link to={`/note/${note.id}`} >Detalle</Link>
+                <h2>{product.title} </h2>
+                <p>{product.category} </p>
+                <p>{product.text} </p>
+                <h3>${product.price} </h3>
+                <Link to={`/product/${product.id}`} >Detalle</Link>
 
             </div>
     
